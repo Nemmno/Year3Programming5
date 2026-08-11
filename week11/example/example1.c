d2d914768856eaaef892798dde00864ccff2af60
Nemmno <67991005@kmitl.ac.th>
2026-08-11 08:47:34 +0700

    example1

--- week11/example/example1.c
+++ week11/example/example1.c
@@ -6,7 +6,7 @@ int main(void){
     if(ptr == NULL) { // Check if memory allocation was successful
         return 1; // Exit the program with an error code
     }
-    *ptr = 2026; // Assign the value 2026 to the allocated memory
+    *ptr = 100; // Assign the value 100 to the allocated memory
     printf("Value stored in heap: %d\n", *ptr);
     free(ptr); // Deallocate the memory
     return 0;
