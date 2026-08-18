a2386c9e723d6adb7515bfcc1e367b4319e1a336
Nemmno <67991005@kmitl.ac.th>
2026-08-18 08:58:04 +0700

    Ex4

---
+++ week12/exercise4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct Employee {
+    int id;
+    float salary;
+};
+
+int main(){
+    struct Employee staff[3] = {
+        {1001, 45000.00},
+        {1002, 52000.00},
+        {1003, 38000.00}
+    };
+
+    for(int i = 0; i < 3; i++){
+        printf("Employee %d -> ID: %d, Salary: $%.2f\n", i + 1, staff[i].id, staff[i].salary);    
+    }
+
+    return 0;
+}
