b38b6e17f0c39783f9e67a31846effe6aedc2504
Nemmno <67991005@kmitl.ac.th>
2026-08-18 08:51:55 +0700

    Ex3

---
+++ week12/exercise3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+int main(){
+    Point p1 = {5, 10};
+
+    printf("P1 Point Coordinates:\n");
+    printf("X: %d\n", p1.x);
+    printf("Y: %d\n", p1.y);
+    Point p2 = p1; // Copying p1 to p2
+    printf("P2 Point Coordinates:\n");
+    printf("X: %d\n", p2.x);
+    printf("Y: %d\n", p2.y);
+
+    return 0;
+}
