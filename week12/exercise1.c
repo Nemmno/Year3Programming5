49ca371d9d250448751568647728ec59dade5509
Nemmno <67991005@kmitl.ac.th>
2026-08-18 08:38:29 +0700

    Ex1

---
+++ week12/exercise1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+struct Student {
+    int id;
+    float gpa;
+};
+
+int main(){
+    struct Student student1 = {101,3.75};
+
+    //printf("\nStudent Information:\n");
+    printf("Student ID : %d\n", student1.id);
+    printf("Student GPA: %.2f\n", student1.gpa);
+
+    return 0;
+}
